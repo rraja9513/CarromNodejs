83951d403d6dec7c59b59a7f61cd0fe4be06794d
Raja R <rraja9513@gmail.com>
2021-07-05 12:40:30 +0530

    forgotpassworddone

--- routes/admin.js
+++ routes/admin.js
@@ -72,7 +72,7 @@ router.route('/login').post((req, res, next)=> {
           subject: 'Node.js Password Admin',
           text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
             'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-            'http://' + req.headers.host + '/reset/' + token + '\n\n' +
+            'https://carrom-admin.herokuapp.com/#/examples/reset/' + token + '\n\n' +
             'If you did not request this, please ignore this email and your password will remain unchanged.\n'
         };
         smtpTransport.sendMail(mailOptions, function(err) {
